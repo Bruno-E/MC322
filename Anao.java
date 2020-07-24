845a890b913574526a51512bc405b1f916fa1405
IkaroB <43428326+IkaroB@users.noreply.github.com>
2020-07-24 01:45:11 -0300

    comentarios e import retirados

--- Anao.java
+++ Anao.java
@@ -1,17 +1,10 @@
 
-import java.util.*;
-/**
- *
- */
 public class Anao extends Heroi {
     private static int VIDA_INICIAL = 7;
     private static int INT_INICIAL = 3;
     private static int ATQ_INICIAL = 2;
     private static int DFS_INICIAL = 2;
 
-    /**
-     * Default constructor
-     */
     public Anao(String nome) {
         super(nome);
         this.vida = VIDA_INICIAL;
